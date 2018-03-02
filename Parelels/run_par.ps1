2aaacd9e9ee89e4b59336f072727c85887659aeb
ArtemPC <khokhlov.artem@gmail.com>
2018-03-02 19:04:32 +0300

    Work paralels

---
+++ Parelels/run_par.ps1
@@ -0,0 +1,13 @@
+﻿       workflow Test-Workflow #($array)
+        {           
+           $array = "C:\1","G:\2","M:\3"
+           ForEach -Parallel ($job in $array)
+           {
+               Parallel
+               {
+                  robocopy.exe "G:\distr\" $job data3.bin /Z /J /MT 30 /log+:$job"\log.log"
+               }
+           }
+        }
+
+        Test-Workflow 
